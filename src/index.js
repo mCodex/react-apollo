da404179271b04b2aeb4d428f770760ea872c148
Mateus Andrade <mat.andrade@live.com>
2018-12-26 11:15:59 -0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Loading default CSS from Semantic UI
+import 'semantic-ui-css/semantic.min.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+ReactDOM.render(
+  <ApolloProvider
+    client={client}
+  >
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
